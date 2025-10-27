2b79343309aab3d584b5842496f83ef343582711
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2025-10-27 14:13:53 +0700

    update ssid name

--- include/wifi_id.h
+++ include/wifi_id.h
@@ -1,4 +1,5 @@
-#define WIFI_SSID "Steff"
-#define WIFI_PASSWORD "Steff123"
-// #define WIFI_SSID "CE-IOT"
-// #define WIFI_PASSWORD "CE-1OT@!"
+// #define WIFI_SSID "Steff"
+// #define WIFI_PASSWORD "Steff123"
+// CE–IOT (-50 dBm) Channel: 1 | Encryption: WPA2-PSK
+#define WIFI_SSID "CE–IOT"
+#define WIFI_PASSWORD "CE-1OT@!"
